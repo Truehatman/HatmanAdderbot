8024c2fd6894ff1d1022d7a46ae766ab7755b55f
hatmanexchange <156363293+hatmanexchange@users.noreply.github.com>
2024-02-20 17:59:57 +0100

    Update HatmanAdderbot.py

--- HatmanAdderbot.py
+++ HatmanAdderbot.py
@@ -14,23 +14,27 @@ def get_user_groups(client):
 # Funzione per ottenere la lista dei membri di un gruppo
 def get_group_members(client, chat_id):
     members = client.get_chat_members(chat_id)
-    return [member.user.username for member in members]
+    return [member.user for member in members]
 
 # Funzione per aggiungere un membro con ritardo
 def add_member_with_delay(client, chat_id, member, delay=60):
     try:
         time.sleep(delay)
-        client.add_chat_members(chat_id, member)
-        print(f"Utente {member} aggiunto con successo al gruppo.")
+        client.add_chat_member(chat_id, user_id=member.id)
+        print(f"Utente {member.username} aggiunto con successo al gruppo.")
         return True
     except Exception as e:
-        print(f"Errore durante l'aggiunta dell'utente {member}: {e}")
+        print(f"Errore durante l'aggiunta dell'utente {member.username}: {e}")
         return False
 
 # Funzione per aggiungere i membri ad un gruppo con ritardo
 def add_members_to_group_with_delay(client, chat_id, members, delay=60):
     for member in members:
-        add_member_with_delay(client, chat_id, member, delay)
+        if not member.is_bot and member.username and member.username.lower() != 'anonymous':
+            # Verifica se l'utente non è un bot e ha un username
+            add_member_with_delay(client, chat_id, member, delay)
+        else:
+            print(f"Skippato l'utente {member.username} nel processo di aggiunta.")
 
 with app:
     # Ottenere la lista dei gruppi dell'account
@@ -49,27 +53,28 @@ with app:
         # Ottieni l'ID del gruppo selezionato
         export_group_id = user_groups[selected_group_num - 1][1]
 
-       
-    print("Lista dei gruppi:")
-    for num, group_id, group_title in user_groups:
-        print(f"{num}.{group_title}")
+        print("Lista dei gruppi:")
+        for num, group_id, group_title in user_groups:
+            print(f"{num}.{group_title}")
 
-    # Chiedi all'utente di scegliere il numero del gruppo in cui importare
-    selected_group_num = int(input("Inserisci il numero del gruppo in cui importare i membri: "))
-    
-    # Verifica se il numero scelto è valido
-    if 1 <= selected_group_num <= len(user_groups):
-        # Ottieni l'ID del gruppo selezionato
-        import_group_id = user_groups[selected_group_num - 1][1]
+        # Chiedi all'utente di scegliere il numero del gruppo in cui importare
+        selected_group_num = int(input("Inserisci il numero del gruppo in cui importare i membri: "))
+        
+        # Verifica se il numero scelto è valido
+        if 1 <= selected_group_num <= len(user_groups):
+            # Ottieni l'ID del gruppo selezionato
+            import_group_id = user_groups[selected_group_num - 1][1]
 
-        # Ottenere la lista dei membri del gruppo di esportazione
-        export_group_members = get_group_members(app, export_group_id)
-        print("Lista dei membri da esportare:")
-        print(export_group_members)
+            # Ottenere la lista dei membri del gruppo di esportazione
+            export_group_members = get_group_members(app, export_group_id)
+            print("Lista dei membri da esportare:")
+            print(export_group_members)
 
-        # Aggiungere i membri al gruppo di importazione con ritardo
-        add_members_to_group_with_delay(app, import_group_id, export_group_members)
+            # Aggiungere i membri al gruppo di importazione con ritardo
+            add_members_to_group_with_delay(app, import_group_id, export_group_members)
 
-        print("Membri aggiunti con successo!")
+            print("Membri aggiunti con successo!")
+        else:
+            print("Numero del gruppo non valido. Per favore, inserisci un numero valido.")
     else:
         print("Numero del gruppo non valido. Per favore, inserisci un numero valido.")
